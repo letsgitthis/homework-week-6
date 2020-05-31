50d7e10623f8ff1697e8628ae2bdbf34bc5f7558
John Bentley <jbentley1011@gmail.com>
2020-05-31 11:49:01 -0500

    trying to refactor and create cards

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,3 @@
-//api key
-// needs the &appid= to work
-const apiKey = "ca7d5beb3ae1753031e2f7307c9df1b8";
 
 let date = new Date();
 let currentHour = moment().hour();
@@ -11,5 +8,166 @@ $(document).ready(function () {
         $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
     }
     setInterval(update, 1000)
+    
+});
 
+$(document).ready(function () {
+    //api key
+    const apiKey = "ca7d5beb3ae1753031e2f7307c9df1b8";
+    let vLon = 0;
+    let vLat = 0;
+
+    let browserHistory = JSON.parse(window.localStorage.getItem("history")) || [];
+    // store search history as array
+    for (let i = 0; i < browserHistory.length; i++) {
+        createButton(browserHistory[i]);
+    }
+
+    $(document).on("click", ".citybtn", function (event) {
+        searchOpenWeather($(this).val());
+    });
+
+    $("#searchBtn").on("click", function (event) {
+        // Preventing the button from trying to submit the form
+        event.preventDefault();
+        // Storing the city name
+        let weatherCity = $("#searchCity").val().trim();
+        console.log(weatherCity);
+        $("#searchCity").val("");
+        // Running the function(passing in the city as an argument)
+        searchOpenWeather(weatherCity);
+    });
+
+    $("#weather-input").keypress;
+
+    // correct this
+    function createButton(city) {
+        let newButton = $("<button>").addClass("btn citybtn").text(city);
+        newButton.val(city);
+        $(".cityList").append(newButton);
+    }
+
+    function searchOpenWeather(city) {
+        let APIKey = "ca7d5beb3ae1753031e2f7307c9df1b8";
+
+        // Here we are building the URL we need to query the database
+        let queryURL =
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&appid=" +
+            APIKey;
+
+        console.log(queryURL);
+        // Here we run our AJAX call to the OpenWeatherMap API
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function (response) {
+                // if statement below prevents duplicate entries before populating the search history
+                if (browserHistory.indexOf(city) === -1) {
+                    browserHistory.push(city);
+                    window.localStorage.setItem(
+                        "history",
+                        JSON.stringify(browserHistory)
+                    );
+                    window.location.reload();
+                }
+
+                // Transfer content to HTML
+                $(".city").html("<h1>" + response.name + "    Weather Details</h1>");
+                $(".wind").text("Wind Speed: " + response.wind.speed);
+                $(".humidity").text("Humidity: " + response.main.humidity);
+
+                // Convert the temp to fahrenheit
+                var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+
+                // add temp content to html
+                $(".temp").text("Temperature (K) " + response.main.temp);
+                $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+                // Log the data in the console as well
+                console.log("Wind Speed: " + response.wind.speed);
+                console.log("Humidity: " + response.main.humidity);
+                console.log("Temperature (F): " + tempF);
+                vLon = response.coord.lon;
+                vLat = response.coord.lat;
+                console.log(vLon, vLat);
+                searchOpenWeatherForecast(city);
+                return vLat, vLon;
+            });
+    }
+
+    function searchOpenWeatherForecast(city) {
+        let APIKey = "ca7d5beb3ae1753031e2f7307c9df1b8";
+        // query the database
+        let queryURL =
+            "https://api.openweathermap.org/data/2.5/forecast?q=" +
+            city +
+            "&appid=" +
+            APIKey;
+
+        console.log(queryURL);
+        // AJAX call to OpenWeatherMap
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+            // store data as response
+            .then(function (response) {
+                // Log the queryURL
+                console.log(queryURL);
+                // Log the resulting object
+                console.log(response);
+
+                $; // push to HTML
+                for (let i = 0; i < 5; i++) {
+                    console.log([i]);
+                    $(".date" + [i]).text("Date " + response.list[i].dt_txt);
+                    $(".wind" + [i]).text("Wind Speed: " + response.list[i].wind.speed);
+                    $(".humidity" + [i]).text(
+                        "Humidity: " + response.list[i].main.humidity
+                    );
+
+                    // Convert to fahrenheit
+                    var tempF = (response.list[0].main.temp - 273.15) * 1.8 + 32;
+
+                    // push to HTML
+                    $(".temp2" + [i]).text(
+                        "Temperature (K) " + response.list[i].main.temp
+                    );
+                    $(".tempF" + [i]).text("Temperature (F) " + tempF.toFixed(2));
+                }
+                console.log(vLat, vLon);
+                searchOpenWeatherUV();
+                // Log the data in the console as well
+                console.log("Wind Speed: " + response.list[0].wind.speed);
+                console.log("Humidity: " + response.list[0].main.humidity);
+                console.log("Temperature (F): " + tempF);
+            });
+    }
+    function searchOpenWeatherUV() {
+        let APIKey = "ca7d5beb3ae1753031e2f7307c9df1b8";
+        // query the database
+        let queryURL =
+            "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+            APIKey +
+            "&lat=" +
+            vLat +
+            "&lon=" +
+            vLon;
+
+        console.log(queryURL);
+        // AJAX call for OpenWeather
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+        })
+            // store data as response
+            .then(function (response) {
+                // push to HTML
+                $(".uvIndex").text("UV Index:" + response.value);
+            });
+    }
 });
